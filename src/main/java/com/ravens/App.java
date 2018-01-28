23326bc7dc42256aa30ba86f3e15401c83d11aef
bjwhorley <bjwhorley@gmail.com>
2018-01-27 21:52:09 -0500

    adding new files

---
+++ src/main/java/com/ravens/App.java
@@ -0,0 +1,55 @@
+package com.ravens;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.util.concurrent.TimeUnit;
+
+public class App
+{
+
+
+
+    public static void main( String[] args ) {
+
+        System.setProperty("webdriver.gecko.driver", "/Users/bwhorley/IdeaProjects/selenium/geckodriver");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(false);
+        WebDriver driver = new FirefoxDriver(options);
+
+        driver.get("http://www.espn.com/");
+        String title = driver.getTitle();
+        String expected = "Nope: The Worldwide Leader in Sports";
+
+        System.out.println(title);
+
+        String pagename = "ESPN";
+
+        boolean status = title.equalsIgnoreCase(expected);
+        System.out.println(status);
+        //boolean titlename =
+
+        if (status == true)
+
+            System.out.println("Yes, it's on Espn ");
+
+        else
+            driver.get("https://www.nfl.com/");
+            driver.quit();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
---
+++ src/main/java/com/ravens/PassParam.java
@@ -0,0 +1,41 @@
+package com.ravens;
+
+public class PassParam {
+
+
+    public static void main(String[] args) {
+
+
+        PassParam sendv = new PassParam();
+        int result =sendv.add(4390,4322);
+        double sum = sendv.substract(45.132, 2898.123);
+        String name = sendv.FullName("Byrn's Birthday is", " August 22nd");
+
+        System.out.println("Birthday: " +name);
+        System.out.println("Value of result is: " +result);
+
+
+        System.out.println("Value of c is: " +sum);
+    }
+
+
+    public int add(int a, int b){
+
+        int result = a + b;
+
+        return result;
+
+    }
+
+    public double substract(double a, double b){
+        double c = b -a;
+        return c;
+
+    }
+
+    public static String FullName(String FirstName, String LastName){
+
+        String fullname = FirstName+LastName;
+        return fullname;
+    }
+}
